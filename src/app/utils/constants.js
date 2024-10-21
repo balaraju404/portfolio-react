4d5a93bc9be754d49eefde0722270ad95048ce34
Balaraju Gandham <balarajugandham1@gmail.com>
2024-10-22 00:07:02 +0530

    Update constants.js

--- src/app/utils/constants.js
+++ src/app/utils/constants.js
@@ -1,5 +1,6 @@
 
 // export const API_ENDPOINT_URL = 'https://restaurant-node-mongo.vercel.app/api/';
-export const API_ENDPOINT_URL = 'http://localhost:3100/api/';
+// export const API_ENDPOINT_URL = 'http://localhost:3100/api/';
+export const API_ENDPOINT_URL = 'https://portfolio-node-khaki.vercel.app/api/'
 export const API_FILE_UPLOAD_URL = 'http://localhost:3100/upload';
 export const LS_USER_DATA_KEY = 'login_user_data'
